287a26eadd2a34ae07adb85b41fdd91518ef64b2
Niek Candaele <22315101+niekcandaele@users.noreply.github.com>
2020-05-08 13:00:12 +0200

    Load Redis connection options properly (#3)

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,10 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import * as Sentry from '@sentry/node';
-import { AppModule } from './app.module';
 import * as dotenv from 'dotenv';
-
 import * as fs from 'fs';
+
+import { AppModule } from './app.module';
 
 dotenv.config();
 const packageJson = JSON.parse(fs.readFileSync('package.json', 'utf8'));
@@ -13,6 +13,6 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
   Sentry.init({ dsn: process.env.BANTR_SENTRY_DSN, release: packageJson.version });
-  await app.listen(3000);
+  await app.listen(6596);
 }
 bootstrap();
--- src/queue/queue.module.ts
+++ src/queue/queue.module.ts
@@ -1,17 +1,30 @@
-import { Module } from '@nestjs/common';
-import { QueueService } from './queue.service';
 import { BullModule } from '@nestjs/bull';
+import { Module } from '@nestjs/common';
+import * as dotenv from 'dotenv';
+
+import { QueueService } from './queue.service';
+
+dotenv.config();
+
+const redisOptions = {
+    host: process.env.BANTR_REDIS_HOST,
+    port: parseInt(process.env.BANTR_REDIS_PORT,10),
+    keyPrefix: 'bantr-koyi'
+}
 
 @Module({
   imports: [
     BullModule.registerQueue({
-      name: 'faceit'
+      name: 'faceit', 
+      redis: redisOptions
     }),
     BullModule.registerQueue({
-      name: 'matches'
+      name: 'matches',
+      redis: redisOptions
     }),
     BullModule.registerQueue({
-      name: 'bans'
+      name: 'bans',
+      redis: redisOptions
     })
   ],
   providers: [QueueService],
