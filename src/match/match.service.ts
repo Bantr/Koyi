39f27df05221881bceec95df2a7a89f73440dc23
Niek Candaele <22315101+niekcandaele@users.noreply.github.com>
2020-06-06 12:27:48 +0200

    Random fixes and work that are not tracked in issues (#13)

--- src/match/match.service.ts
+++ src/match/match.service.ts
@@ -1,4 +1,3 @@
-import { IMatchType } from '@bantr/lib/dist/types';
 import { OnQueueCompleted, OnQueueError, OnQueueFailed, OnQueueProgress, Process, Processor } from '@nestjs/bull';
 import { HttpService, Injectable, Logger } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
@@ -14,6 +13,7 @@ import { QueueService } from '../queue/queue.service';
 import { UserRepository } from '../user/user.repository';
 import Demo from './demo';
 import { CsgoMatchDto } from './dto/csgoMatch.dto';
+import Match from './match.entity';
 import { MatchRepository } from './match.repository';
 
 // import Demo from './demo';
@@ -60,12 +60,18 @@ export class MatchService {
    */
 
   public async addMatchToQueue(data: CsgoMatchDto) {
-    const exists = await this.checkIfExists(data.type, data.externalId);
+    const exists = await this.matchRepository.checkIfExists(data.externalId);
 
     if (exists) {
       // If the match is already handled, discard the job
       return;
     }
+
+    const match = new Match();
+    match.externalId = data.externalId;
+    match.demoUrl = data.demoUrl;
+    match.date = new Date();
+    await this.matchRepository.createMatch(match);
 
     this.logger.debug(
       `addMatchToQueue() - Handling a new match of type ${data.type}`
@@ -78,16 +84,9 @@ export class MatchService {
    * Transfroms the data gotten from the source into something useable by the system
    * @param job
    */
-  @Process({ name: '__default__', concurrency: 5 })
+  @Process({ name: '__default__' })
   async handleMatch(job: Job) {
     const data = job.data as CsgoMatchDto;
-
-    const exists = await this.checkIfExists(data.type, data.externalId);
-
-    if (exists) {
-      // If the match is already handled, discard the job
-      return;
-    }
 
     await job.progress(0.25);
 
@@ -108,11 +107,17 @@ export class MatchService {
         return {
           steamId: _.steamId
         };
-      })
+      }),
+      relations: ['tracks']
     });
 
     for (const user of usersInMatch) {
-      user.tracks = match.players;
+      for (const player of match.players) {
+        // Only push players into array that are unique
+        if (user.tracks.indexOf(player) === -1) {
+          user.tracks.push(player);
+        }
+      }
       await user.save();
     }
 
@@ -146,14 +151,8 @@ export class MatchService {
    */
   private async handleDemo(matchBuffer: Buffer, matchData: CsgoMatchDto) {
     const demo = new Demo(matchBuffer, this.connection);
-    return await demo.handle(matchData);
-  }
-
-  async checkIfExists(type: IMatchType, externalId: string): Promise<boolean> {
-    const matches = await this.matchRepository.find({
-      where: { type, externalId }
-    });
-    return !!matches.length;
+    const matchRecord = await this.matchRepository.findMatchByExternalId(matchData.externalId);
+    return await demo.handle(matchData, matchRecord);
   }
 
   @OnQueueProgress()
@@ -178,7 +177,7 @@ export class MatchService {
   @OnQueueFailed()
   onFailed(job: Job, err: Error) {
     this.logger.error(
-      `Job ${job.id} of type ${job.name} from queue ${job.queue.name} has failed!`
+      `Job ${job.id} of type ${job.name} from queue ${job.queue.name} has failed!`, err.stack
     );
     Sentry.captureException(err);
   }
