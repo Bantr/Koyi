8e50d9c75351c81e20844135faf6409400b2ac91
niekcandaele <22315101+niekcandaele@users.noreply.github.com>
2020-06-09 12:59:06 +0200

    Add test to check correct amount of players detected

--- src/match/demo.spec.ts
+++ src/match/demo.spec.ts
@@ -1,20 +1,71 @@
+import { IMatchType } from '@bantr/lib/dist/types';
+import { Test, TestingModule } from '@nestjs/testing';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import * as dotenv from 'dotenv';
 import * as fs from 'fs';
 import * as path from 'path';
 
 import Demo from './demo';
+import Match from './match.entity';
+
+dotenv.config();
+
+// Database connection is required to run this test!
 
 describe('Demo handler', () => {
+  // Timeout reaaaally long because parsing a demo can take a while
+  // Especially on slow CI servers
+  jest.setTimeout(1000 * 60 * 5);
+
   let demoClass: Demo;
   let demoFileBuffer: Buffer;
+  let app: TestingModule;
+  let resultMatch: Match;
 
-  beforeAll(() => {
+  beforeAll(async () => {
     demoFileBuffer = fs.readFileSync(
       path.join(__dirname, '../../test/demos/faceit-5v5.dem')
     );
     demoClass = new Demo(demoFileBuffer);
+
+    app = await Test.createTestingModule({
+      imports: [
+        TypeOrmModule.forRoot({
+          type: 'postgres',
+          host: process.env.BANTR_PG_HOST,
+          port: parseInt(process.env.BANTR_PG_PORT, 10),
+          username: process.env.BANTR_PG_USER,
+          password: process.env.BANTR_PG_PW,
+          database: process.env.BANTR_PG_DB,
+          entities: [__dirname + '/../**/*.entity{.ts,.js}'],
+          synchronize: true
+        })
+      ]
+    }).compile();
+
+    const initMatch = new Match();
+    initMatch.externalId = 'Testing Demo 5v5 faceit';
+    resultMatch = await demoClass.handle(
+      {
+        externalId: 'foo',
+        id: ' bar',
+        type: IMatchType.CSGOFaceIt,
+        demoUrl: 'notneeded'
+      },
+      initMatch
+    );
   });
 
   it('is defined', () => {
     expect(demoClass).toBeDefined();
+  });
+
+  it('returns a correct amount of players', async () => {
+    expect(resultMatch).toHaveProperty('players');
+    expect(resultMatch.players).toHaveLength(10);
+  });
+
+  afterAll(async () => {
+    await app.close();
   });
 });
--- src/match/match.type.interface.ts
+++
@@ -1,4 +0,0 @@
-export enum MatchType {
-    CSGOFaceItMatchmaking = 0,
-    CSGOMatchMaking,
-}
